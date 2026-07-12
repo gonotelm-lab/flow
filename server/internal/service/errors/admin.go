a421bf9ce32e06e2d3d4ade1966fec2b13e793f9
ryanreadbooks <ryanreadbooks@foxmail.com>
2026-07-12 14:40:03 +0800

    feat(service): add WorkerNotFound error definition

--- server/internal/service/errors/admin.go
+++ server/internal/service/errors/admin.go
@@ -8,9 +8,11 @@ import (
 const (
 	KeyNamespaceNotFound pkgerr.DomainErrorKey = "NAMESPACE_NOT_FOUND"
 	KeyNamespaceExists   pkgerr.DomainErrorKey = "NAMESPACE_ALREADY_EXISTS"
+	KeyWorkerNotFound    pkgerr.DomainErrorKey = "WORKER_NOT_FOUND"
 )
 
 var (
 	NamespaceNotFound = pkgerr.New(codes.NotFound, KeyNamespaceNotFound)
 	NamespaceExists   = pkgerr.New(codes.AlreadyExists, KeyNamespaceExists)
+	WorkerNotFound    = pkgerr.New(codes.NotFound, KeyWorkerNotFound)
 )
